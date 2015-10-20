f671073a239e8f2b02aa4a4941c5428077825c69
Rob Fisher <me@robfisher.us>
2015-10-20 13:09:02 -0400

    Getting parity with old site

---
+++ client/templates/sections/next-meeting.js
@@ -0,0 +1,25 @@
+function getDate(month) {
+  var result = month.endOf('month');
+  while(result.day() !== 1)
+    result.subtract(1, 'day');
+
+  return result;
+}
+
+Template.nextMeeting.onRendered(function(){
+  var result = getDate(moment());
+  var dur = result.diff(moment());
+
+  if (dur < 0)
+    result = getDate(moment().add(1, 'month'));
+
+  var meeting = result.subtract(5.5, 'hours');
+  meeting.add(1, 'second');
+
+  $('.next').text(meeting.format('dddd, MMMM Do YYYY, h:mm:ss a') + " EST");
+
+  $('.countdown').downCount({
+    date: meeting.add(4, 'hours').format(),
+    offset: 0
+  });
+});
